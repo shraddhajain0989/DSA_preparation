118fbc3d86d83e55d0706ec848f80ed1891b5bf2
Shraddha <shraddhajain0989@gmail.com>
2026-01-07 23:29:08 +0530

    smallest and simple c programme

---
+++ reverse_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char s[]) {
+    int l = 0, r = strlen(s) - 1;
+
+    while (l < r) {
+        char temp = s[l];
+        s[l] = s[r];
+        s[r] = temp;
+        l++;
+        r--;
+    }
+}
+
+int main() {
+    char s[] = "hello";
+    reverseString(s);
+    printf("%s", s);
+    return 0;
+}
