7df6e71aa1d2dccd90035a408f3d9e8f8c13fa2b
Shraddha <shraddhajain0989@gmail.com>
2026-02-05 22:50:36 +0530

    Rotate an Array by One Position

---
+++ Rotate an Array by One Position.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int first = arr[0];
+
+    for (int i = 0; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    arr[n - 1] = first;
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
